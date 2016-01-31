aedc032c59f141a8e3e369f780cddcfa4e389717
Chakshu Ahuja <ahuja.chaks@gmail.com>
2016-01-31 13:37:53 +0530

    Deunicodify the json

---
+++ beautify.py
@@ -0,0 +1,12 @@
+import json
+def deunicodify_hook(pairs):
+    new_pairs = []
+    for key, value in pairs:
+        if isinstance(value, unicode):
+            value = value.encode('utf-8')
+        if isinstance(key, unicode):
+            key = key.encode('utf-8')
+        new_pairs.append((key, value))
+    return dict(new_pairs)
+
+print json.dumps(json.load(open('tweets_data.json'), object_pairs_hook=deunicodify_hook))
